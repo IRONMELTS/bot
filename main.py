ab6f5f7b4e2a5347c23705568dc0e89c152fc6a5
ironmelts <87653130+IRONMELTS@users.noreply.github.com>
2023-12-21 18:17:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+import time
 import os
 from discord.ext import commands
 
@@ -6,14 +7,36 @@ intents=discord.Intents.all()
 
 bot=commands.Bot(command_prefix='.',intents=intents)
 
-@bot.event
-async def on_ready():
-    print("BOT is ready")
-    status=discord.Game(name="sleeping")
-    await bot.change_presence(status=discord.Status.idle,activity=status)
+reply_id={}
 
+@bot.event
+
+async def on_ready():
+
+    print("BOT is ready")
+    await bot.change_presence(activity=discord.Game(name="~~~ready~~~"))
+    time.sleep(3)
+    await bot.change_presence(activity=discord.Game(name=".help"))
+
+    
 @bot.command(name="ping")
+
 async def ping(ctx):
+
     await ctx.send(f"{round(bot.latency*1000)}ms")
 
+@bot.command(name="user")
+
+async def user(ctx,user_id:int):
+
+    user=await bot.fetch_user(user_id)
+    await ctx.send(f"Account name: `{user.name}`\nAccount created on: `{str(user.created_at)[0:-13]}`")
+
+@bot.command(name="av")
+
+async def av(ctx,user_id:int):
+
+    user=await bot.fetch_user(user_id)
+    await ctx.send(user.avatar)
+    
 bot.run(os.environ["token"])
